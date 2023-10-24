52450f8b5920969dbdae93208f94136535ad160a
hiennghia11 <lamhiennghia1110@gmail.com>
2023-10-24 20:56:55 +0700

    first comment

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { onMounted } from "vue";
+const body = document.querySelector("body");
+const onLight = () => {
+  body.style.backgroundColor = "white";
+  body.style.color = "black";
+};
+</script>
+
+<template>
+  <div>
+    <button @click="onLight">LIGHTNING</button>
+    <i class="fa-solid fa-book"></i
+    ><span
+      >Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel doloremque
+      distinctio possimus culpa. Reprehenderit dolorum illo ex error omnis
+      molestias eius tempora, optio aut fuga amet incidunt quaerat, veniam
+      repellendus!</span
+    >
+  </div>
+</template>
+
+<style scoped></style>
